66c09205d39c7721fb1183159a4c54666323f86d
noteax <aidarbariev@gmail.com>
2016-09-30 17:24:30 +0300

    Futher development

--- src/main/resources/static/js/advertDetail.js
+++ src/main/resources/static/js/advertDetail.js
@@ -3,17 +3,8 @@
     'use strict';
 
     $(document).ready(function () {
-        $("#advert-carousel").owlCarousel({
-            autoPlay: 3000,
-            stopOnHover: true,
-            navigation: true,
-            paginationSpeed: 1000,
-            goToFirstSpeed: 2000,
-            autoWidth: true,
-            loop: true,
-            margin: 10,
-            items: 3,
-            transitionStyle: "fade"
+        $('#adverts-gallery').lightGallery({
+            selector: '.item'
         });
     });
 }());
