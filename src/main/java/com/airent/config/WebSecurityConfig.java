6ef34d95ea69db85523726fe227a9b7d4426249e
noteax <aidarbariev@gmail.com>
2016-10-31 08:13:12 +0300

    Futher development of security

--- src/main/java/com/airent/config/WebSecurityConfig.java
+++ src/main/java/com/airent/config/WebSecurityConfig.java
@@ -1,16 +1,37 @@
 package com.airent.config;
 
 
+import com.airent.security.AuthFailureHandler;
+import com.airent.security.AuthSuccessHandler;
+import com.airent.security.HttpAuthenticationEntryPoint;
+import com.airent.security.HttpLogoutSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private HttpAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private AuthSuccessHandler authSuccessHandler;
+    @Autowired
+    private AuthFailureHandler authFailureHandler;
+    @Autowired
+    private HttpLogoutSuccessHandler logoutSuccessHandler;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -19,16 +40,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/css/**", "/js/**", "/fonts/**", "/images/**").permitAll()
-                .antMatchers("/user/**").authenticated()
-               // .antMatchers("/advert/**").authenticated()
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/fonts/**", "/images/**")
+                .permitAll()
+                .antMatchers("/user/**")
+                .authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
                 .permitAll()
+                .loginProcessingUrl("/login")
+                .loginPage("/login-page")
+                .successHandler(authSuccessHandler)
+                .usernameParameter("user")
+                .passwordParameter("password")
+                .successHandler(authSuccessHandler)
+                .failureHandler(authFailureHandler)
                 .and()
                 .logout()
-                .permitAll().and().csrf().disable();
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/login", "DELETE"))
+                .logoutSuccessHandler(logoutSuccessHandler)
+                .and()
+                .csrf()
+                .disable();
     }
 }
