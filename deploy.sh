fa4cec934d53a97f6d8483ca524e8b15071a034a
noteax <aidarbariev@gmail.com>
2017-02-02 11:44:55 +0300

    CD - logs/dirs/ssh/commands

--- deploy.sh
+++ deploy.sh
@@ -1,60 +1,84 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Args:
+# 1. - docker user
+# 2. - docker pass
 
 # Continuous deployment script (use ansible instead?)
 # Runs step by step:
-#  - Deploy/start postgres image
-#  - Configures backup
-#  - Stop app image
-#  - Deploy/Start app image
-#  - Report about statuses
-sudo su
+#  - 1. Get root access/configure/login into docker
+#  - 2. Deploy/start postgres image
+#  - 3. Configures backup
+#  - 4. Stop app image
+#  - 5. Deploy/Start app image
+#  - 6. Report about statuses
 
-pgImageName=postgres-oyouin
+export dockerUser=$1
+export dockerPassword=$2
+
+### -- Step 1 -- ###
+sudo -E su
+
+pgContainerName=postgres-oyouin
+pgImageName=postgres:9.4
 pgDataPath=/data/postgres
 pgBackupPath=/data/backup/postgres
 
+appContainerName=oyouin-main
 appImageName=oyouin/main
 appStaticImagePath=/data/photos
+appLogPath=/data/logs
 
-# Deploy/start postgres image
-mkdir -p ${pgDataPath}
-if 0 -eq docker ps | grep -q ${pgImageName}
+echo "Login to docker hub as ${dockerUser}."
+docker login -u ${dockerUser} -p ${dockerPassword}
+
+### -- Step 2 -- ###
+if ! docker ps | grep -q ${pgImageName}
 then
-  docker run --name ${pgImageName} \
-           postgresql:9.4 \
+  echo "Run docker postgres image"
+  docker run --name ${pgContainerName} \
            -p 4466:5432 \
            -e POSTGRES_PASSWORD=AQGnthVu73AjBfBF \
-           -d postgres \
-           -v ${pgDataPath}:/var/lib/postgresql/data
+           -v ${pgDataPath}:/var/lib/postgresql/data \
+           -d \
+           ${pgImageName}
 fi
 
-# !!!! Configure backup for postgres
+### -- Step 3 -- ###
 
-# Stop app image
-if 0 -eq docker ps | grep -q ${appImageName}
+### -- Step 4 -- ###
+if docker ps -a | grep -q ${appContainerName}
 then
-  docker stop ${appImageName}
+  echo "Stop & remove app container"
+  docker stop ${appContainerName}
+  docker rm ${appContainerName}
 fi
 
-# Deploy/Start app image
-docker start ${appImageName} \
-          -v ${appStaticImagePath}:/photos
+### -- Step 5 -- ###
+echo "Run app container"
+docker run --name ${appContainerName} \
+         -p 3212:8080 \
+         -e LOG_DIR=/logs \
+         -v ${appStaticImagePath}:/photos \
+         -v ${appLogPath}:/logs \
+         -d \
+         ${appImageName}
 
-# Report statuses
+### -- Step 6 -- ###
 # Check postgres connectivity at first
 # Check app connectivity at second
-echo "Checking postgresql connectivity ... "
-if 0 -eq lsof -i :4466 -P -n  | grep -q 4466
-then
- echo "OK"
-else
- echo "Failed"
-fi
-
-echo "Checking app connectivity ... "
-if 0 -eq lsof -i :8080 -P -n  | grep -q 8080
-then
- echo "OK"
-else
- echo "Failed"
-fi
+#echo "Checking postgresql connectivity ... "
+#if ! lsof -i :4466 -P -n  | grep -q 4466
+#then
+# echo "OK"
+#else
+# echo "Failed"
+#fi
+#
+#echo "Checking app connectivity ... "
+#if ! lsof -i :8080 -P -n  | grep -q 8080
+#then
+# echo "OK"
+#else
+# echo "Failed"
+#fi
