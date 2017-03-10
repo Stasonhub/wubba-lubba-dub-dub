6d2aa0bab31c488238d23264e9c9cf9318ca6f6b
noteax <aidarbariev@gmail.com>
2017-03-10 12:42:52 +0300

    Added site.xml generation, minor fixes in robots.txt

---
+++ src/main/java/com/airent/controller/SitemapController.java
@@ -0,0 +1,95 @@
+package com.airent.controller;
+
+import com.airent.mapper.AdvertMapper;
+import com.airent.model.Advert;
+import com.redfin.sitemapgenerator.ChangeFreq;
+import com.redfin.sitemapgenerator.WebSitemapGenerator;
+import com.redfin.sitemapgenerator.WebSitemapUrl;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Controller
+public class SitemapController {
+
+    private Logger logger = LoggerFactory.getLogger(SitemapController.class);
+
+    @Value("${domain.link}")
+    private String domainLink;
+
+    @Autowired
+    private AdvertMapper advertMapper;
+
+    private Lock lock = new ReentrantLock();
+
+    @RequestMapping(method = RequestMethod.GET, path = "/sitemap.xml")
+    public void getSitemap(HttpServletResponse response) throws IOException {
+        lock.lock();
+        try {
+            File file = new File(System.getProperty("java.io.tmpdir") + File.separator + "/ftr");
+            file.mkdirs();
+
+            File sitemap = new File(file, "sitemap.xml");
+
+            if (!sitemap.exists() || sitemap.lastModified() < (System.currentTimeMillis() - 3600000)) {
+                WebSitemapGenerator wsg = new WebSitemapGenerator(domainLink, file);
+                wsg.addUrl(new WebSitemapUrl.Options(domainLink)
+                        .lastMod(new Date())
+                        .priority(1.0)
+                        .changeFreq(ChangeFreq.HOURLY)
+                        .build());
+                wsg.addUrl(new WebSitemapUrl.Options(domainLink + "/search/?districts=AV,VH,KR,MS,NS,PV,CV&rooms1=false&rooms2=false&rooms3=false&priceRange=4,100")
+                        .lastMod(new Date())
+                        .priority(1.0)
+                        .changeFreq(ChangeFreq.HOURLY)
+                        .build());
+
+                // fill sitemap by batches
+                while (true) {
+                    List<Advert> adverts = advertMapper.getNextAdvertsBeforeTime(0L, 100);
+                    for (Advert advert : adverts) {
+                        wsg.addUrl(new WebSitemapUrl.Options(domainLink + "/advert/" + advert.getId())
+                                .lastMod(new Date(advert.getPublicationDate()))
+                                .priority(0.5)
+                                .changeFreq(ChangeFreq.MONTHLY)
+                                .build());
+                    }
+                    if (adverts.isEmpty()) {
+                        break;
+                    }
+                }
+
+                List<File> sitemapFiles = wsg.write();
+                if (sitemapFiles.size() != 1) {
+                    logger.warn("Failed to generate sitemap.xml. Files: {}", sitemapFiles);
+                    return;
+                }
+                sitemap = sitemapFiles.get(0);
+            }
+
+            try (InputStream sitemapStream = FileUtils.openInputStream(sitemap)){
+                response.setContentType("application/xml");
+                IOUtils.copy(sitemapStream, response.getOutputStream());
+                response.flushBuffer();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
