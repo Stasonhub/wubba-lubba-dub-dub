64397645da72c4f1587e8fb1b0f241bed1306244
noteax <aidarbariev@gmail.com>
2017-06-04 17:24:10 +0300

    Fixed logging, CI build file and app pid

--- web-server/run-app.sh
+++ web-server/run-app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export DISPLAY=:99
 Xvfb -ac :99 -screen 0 1280x1024x16 &
-/breezy-web-server-1.0-SNAPSHOT/bin/breezy-web-server
+/breezy-web-server-1.0-SNAPSHOT/bin/breezy-web-server -Dpidfile.path=/var/run/play.pid
